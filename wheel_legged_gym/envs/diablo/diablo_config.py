20b1244049ec86829718b95bac77742bce5afb06
lsy <2028238109@qq.com>
2024-09-10 14:53:07 +0800

    Open a new config for diablo.

--- wheel_legged_gym/envs/diablo/diablo_config.py
+++ wheel_legged_gym/envs/diablo/diablo_config.py
@@ -28,16 +28,85 @@
 #
 # Copyright (c) 2021 ETH Zurich, Nikita Rudin
 
-from wheel_legged_gym.envs.base.legged_robot_config import (
-    LeggedRobotCfg,
-    LeggedRobotCfgPPO,
-)
+from wheel_legged_gym.envs.base.base_config import BaseConfig
 
 
-class DiabloCfg(LeggedRobotCfg):
+class DiabloCfg(BaseConfig):
+    class env:
+        num_envs = 4096
+        num_observations = 27
+        num_privileged_obs = (
+                num_observations + 7 * 11 + 3 + 6 * 5 + 3 + 3
+        )  # if not None a priviledge_obs_buf will be returned by step() (critic obs for assymetric training). None is returned otherwise
+        obs_history_length = 5  # number of observations stacked together
+        obs_history_dec = 1
+        num_actions = 6
+        env_spacing = 3.0  # not used with heightfields/trimeshes
+        send_timeouts = True  # send time out information to the algorithm
+        episode_length_s = 20  # episode length in seconds
+        dof_vel_use_pos_diff = True
+        fail_to_terminal_time_s = 1
 
-    class init_state(LeggedRobotCfg.init_state):
+    class terrain:
+        mesh_type = "plane"
+        # mesh_type = "trimesh"  # "heightfield" # none, plane, heightfield or trimesh
+        horizontal_scale = 0.1  # [m]
+        vertical_scale = 0.005  # [m]
+        border_size = 25  # [m]
+        curriculum = True
+        static_friction = 0.5
+        dynamic_friction = 0.5
+        restitution = 0.5
+        # rough terrain only:
+        measure_heights = True
+        measured_points_x = [
+            -0.5,
+            -0.4,
+            -0.3,
+            -0.2,
+            -0.1,
+            0.0,
+            0.1,
+            0.2,
+            0.3,
+            0.4,
+            0.5,
+        ]  # 1mx1.6m rectangle (without center line)
+        measured_points_y = [-0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3]
+        selected = False  # select a unique terrain type and pass all arguments
+        terrain_kwargs = None  # Dict of arguments for selected terrain
+        max_init_terrain_level = 5  # starting curriculum state
+        terrain_length = 8.0
+        terrain_width = 8.0
+        num_rows = 10  # number of terrain rows (levels)
+        num_cols = 20  # number of terrain cols (types)
+        # terrain types: [smooth slope, rough slope, stairs up, stairs down, discrete]
+        terrain_proportions = [0.2, 0.2, 0.2, 0.1, 0.2, 0.1]
+        # trimesh only:
+        slope_treshold = (
+            0.75  # slopes above this threshold will be corrected to vertical surfaces
+        )
+
+    class commands:
+        curriculum = True
+        basic_max_curriculum = 2.5
+        advanced_max_curriculum = 1.5
+        curriculum_threshold = 0.7
+        num_commands = 3  # default: lin_vel_x, lin_vel_y, ang_vel_yaw, heading (in heading mode ang_vel_yaw is recomputed from heading error)
+        resampling_time = 5.0  # time before command are changed[s]
+        heading_command = True  # if true: compute ang vel command from heading error
+
+        class ranges:
+            lin_vel_x = [-1.0, 1.0]  # min max [m/s]
+            ang_vel_yaw = [-3.14, 3.14]  # min max [rad/s]
+            height = [0.18, 0.28]
+            heading = [-3.14, 3.14]
+
+    class init_state:
         pos = [0.0, 0.0, 0.35]  # x,y,z [m]
+        rot = [0.0, 0.0, 0.0, 1.0]  # x,y,z,w [quat]
+        lin_vel = [0.0, 0.0, 0.0]  # x,y,z [m/s]
+        ang_vel = [0.0, 0.0, 0.0]  # x,y,z [rad/s]
         default_joint_angles = {  # target angles when action = 0.0
             "left_hip_joint": 0.65,
             "left_knee_joint": -1.25,
@@ -47,16 +116,19 @@ class DiabloCfg(LeggedRobotCfg):
             "right_wheel_joint": 0.0,
         }
 
-    class terrain(LeggedRobotCfg.terrain):
-        mesh_type = "plane"
-    class control(LeggedRobotCfg.control):
-        pos_action_scale = 0.5
-        vel_action_scale = 10.0
+    class control:
+        control_type = "P"  # P: position, V: velocity, T: torques
         # PD Drive parameters:
         stiffness = {"hip": 30.0, "knee": 30.0, "wheel": 0}  # [N*m/rad]
         damping = {"hip": 0.5, "knee": 0.5, "wheel": 0.8}  # [N*m*s/rad]
+        # action scale: target angle = actionScale * action + defaultAngle
+        action_scale = 0.5
+        # decimation: Number of control action updates @ sim DT per policy DT
+        decimation = 2
+        pos_action_scale = 0.5
+        vel_action_scale = 10.0
 
-    class asset(LeggedRobotCfg.asset):
+    class asset:
         file = "{WHEEL_LEGGED_GYM_ROOT_DIR}/resources/robots/diablo/urdf/diablo_issac_gym.urdf"
         name = "diablo"
         offset = 0.
@@ -66,13 +138,179 @@ class DiabloCfg(LeggedRobotCfg):
         terminate_after_contacts_on = ["base"]
         self_collisions = 1  # 1 to disable, 0 to enable...bitwise filter
         flip_visual_attachments = False
+        foot_name = "None"  # name of the feet bodies, used to index body state and contact force tensors
+        disable_gravity = False
+        collapse_fixed_joints = True  # merge bodies connected by fixed joints. Specific fixed joints can be kept by adding " <... dont_collapse="true">
+        fix_base_link = False  # fixe the base of the robot
+        default_dof_drive_mode = 3  # see GymDofDriveModeFlags (0 is none, 1 is pos tgt, 2 is vel tgt, 3 effort)
+        replace_cylinder_with_capsule = True  # replace collision cylinders with capsules, leads to faster/more stable simulation
 
-    class rewards(LeggedRobotCfg.rewards):
-        class scales(LeggedRobotCfg.rewards.scales):
+        density = 0.001
+        angular_damping = 0.0
+        linear_damping = 0.0
+        max_angular_velocity = 1000.0
+        max_linear_velocity = 1000.0
+        armature = 0.0
+        thickness = 0.01
+
+    class domain_rand:
+        randomize_friction = True
+        friction_range = [0.1, 2.0]
+        randomize_restitution = True
+        restitution_range = [0.0, 1.0]
+        randomize_base_mass = True
+        added_mass_range = [-2.0, 3.0]
+        randomize_inertia = True
+        randomize_inertia_range = [0.8, 1.2]
+        randomize_base_com = True
+        rand_com_vec = [0.05, 0.05, 0.05]
+        push_robots = True
+        push_interval_s = 7
+        max_push_vel_xy = 2.0
+        randomize_Kp = True
+        randomize_Kp_range = [0.9, 1.1]
+        randomize_Kd = True
+        randomize_Kd_range = [0.9, 1.1]
+        randomize_motor_torque = True
+        randomize_motor_torque_range = [0.9, 1.1]
+        randomize_default_dof_pos = True
+        randomize_default_dof_pos_range = [-0.05, 0.05]
+        randomize_action_delay = True
+        delay_ms_range = [0, 10]
+
+    class rewards:
+        class scales:
+            tracking_lin_vel = 1.0
+            tracking_lin_vel_enhance = 1
+            tracking_ang_vel = 1.0
+
+            base_height = 1.0
             nominal_state = -0.1
-            # action_smooth = -0.05
+            lin_vel_z = -2.0
+            ang_vel_xy = -0.05
+            orientation = -10.0
 
-class DiabloCfgPPO(LeggedRobotCfgPPO):
-    class runner(LeggedRobotCfgPPO.runner):
+            dof_vel = -5e-5
+            dof_acc = -2.5e-7
+            torques = -0.0001
+            action_rate = -0.01
+            action_smooth = -0.01
+
+            collision = -1.0
+            dof_pos_limits = -1.0
+
+        only_positive_rewards = False  # if true negative total rewards are clipped at zero (avoids early termination problems)
+        clip_single_reward = 1
+        tracking_sigma = 0.25  # tracking reward = exp(-error^2/sigma)
+        soft_dof_pos_limit = (
+            0.97  # percentage of urdf limits, values above this limit are penalized
+        )
+        soft_dof_vel_limit = 1.0
+        soft_torque_limit = 1.0
+        base_height_target = 0.25
+        max_contact_force = 100.0  # forces above this value are penalized
+
+    class normalization:
+        class obs_scales:
+            lin_vel = 2.0
+            ang_vel = 0.25
+            dof_pos = 1.0
+            dof_vel = 0.05
+            dof_acc = 0.0025
+            height_measurements = 5.0
+            torque = 0.05
+
+        clip_observations = 100.0
+        clip_actions = 100.0
+
+    class noise:
+        add_noise = True
+        noise_level = 1.0  # scales other values
+
+        class noise_scales:
+            dof_pos = 0.01
+            dof_vel = 1.5
+            lin_vel = 0.1
+            ang_vel = 0.2
+            gravity = 0.05
+            height_measurements = 0.1
+
+    # viewer camera:
+    class viewer:
+        ref_env = 0
+        pos = [0, -2, 1]  # [m]
+        lookat = [0, 0, 0]  # [m]
+
+    class sim:
+        dt = 0.005
+        substeps = 1
+        gravity = [0.0, 0.0, -9.81]  # [m/s^2]
+        up_axis = 1  # 0 is y, 1 is z
+
+        class physx:
+            num_threads = 10
+            solver_type = 1  # 0: pgs, 1: tgs
+            num_position_iterations = 4
+            num_velocity_iterations = 0
+            contact_offset = 0.01  # [m]
+            rest_offset = 0.0  # [m]
+            bounce_threshold_velocity = 0.5  # 0.5 [m/s]
+            max_depenetration_velocity = 1.0
+            max_gpu_contact_pairs = 2**23  # 2**24 -> needed for 8000 envs and more
+            default_buffer_size_multiplier = 5
+            contact_collection = (
+                2  # 0: never, 1: last sub-step, 2: all sub-steps (default=2)
+            )
+
+
+class DiabloCfgPPO(BaseConfig):
+    seed = 1
+    runner_class_name = "OnPolicyRunner"
+
+    class policy:
+        init_noise_std = 0.5
+        actor_hidden_dims = [128, 64, 32]
+        critic_hidden_dims = [256, 128, 64]
+        activation = "elu"  # can be elu, relu, selu, crelu, lrelu, tanh, sigmoid
+
+        # only for ActorCriticSequence
+        num_encoder_obs = (
+                DiabloCfg.env.obs_history_length * DiabloCfg.env.num_observations
+        )
+        latent_dim = 3  # at least 3 to estimate base linear velocity
+        encoder_hidden_dims = [128, 64]
+
+    class algorithm:
+        # training params
+        value_loss_coef = 1.0
+        use_clipped_value_loss = True
+        clip_param = 0.2
+        entropy_coef = 0.01
+        num_learning_epochs = 5
+        num_mini_batches = 4  # mini batch size = num_envs*nsteps / nminibatches
+        learning_rate = 1.0e-3  # 5.e-4
+        schedule = "adaptive"  # could be adaptive, fixed
+        gamma = 0.99
+        lam = 0.95
+        desired_kl = 0.005
+        max_grad_norm = 1.0
+
+        extra_learning_rate = 1e-3
+
+    class runner:
+        policy_class_name = (
+            "ActorCriticSequence"  # could be ActorCritic, ActorCriticSequence
+        )
+        algorithm_class_name = "PPO"
+        num_steps_per_env = 48  # per iteration
+        max_iterations = 5000  # number of policy updates
+
         # logging
-        experiment_name = "diablo"
+        save_interval = 100  # check for potential saves every this many iterations
+        experiment_name = "test"
+        run_name = ""
+        # load and resume
+        resume = False
+        load_run = -1  # -1 = last run
+        checkpoint = -1  # -1 = last saved model
+        resume_path = None  # updated from load_run and chkpt
