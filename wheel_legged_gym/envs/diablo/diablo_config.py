a7bc68cb00abad141cf4a819be4c166d997e8821
lsy <2028238109@qq.com>
2024-09-10 14:40:58 +0800

    Could train move.

--- wheel_legged_gym/envs/diablo/diablo_config.py
+++ wheel_legged_gym/envs/diablo/diablo_config.py
@@ -28,41 +28,51 @@
 #
 # Copyright (c) 2021 ETH Zurich, Nikita Rudin
 
-from wheel_legged_gym import WHEEL_LEGGED_GYM_ROOT_DIR, WHEEL_LEGGED_GYM_ENVS_DIR
-from .base.legged_robot import LeggedRobot
-from .diablo.diablo import Diablo
-from .wheel_legged.wheel_legged_config import WheelLeggedCfg, WheelLeggedCfgPPO
-from .diablo.diablo_config import DiabloCfg, DiabloCfgPPO
-from .wheel_legged_vmc.wheel_legged_vmc import LeggedRobotVMC
-from .wheel_legged_vmc.wheel_legged_vmc_config import (
-    WheelLeggedVMCCfg,
-    WheelLeggedVMCCfgPPO,
-)
-from .wheel_legged_vmc_flat.wheel_legged_vmc_flat_config import (
-    WheelLeggedVMCFlatCfg,
-    WheelLeggedVMCFlatCfgPPO,
+from wheel_legged_gym.envs.base.legged_robot_config import (
+    LeggedRobotCfg,
+    LeggedRobotCfgPPO,
 )
 
 
-import os
+class DiabloCfg(LeggedRobotCfg):
 
-from wheel_legged_gym.utils.task_registry import task_registry
+    class init_state(LeggedRobotCfg.init_state):
+        pos = [0.0, 0.0, 0.35]  # x,y,z [m]
+        default_joint_angles = {  # target angles when action = 0.0
+            "left_hip_joint": 0.65,
+            "left_knee_joint": -1.25,
+            "left_wheel_joint": 0.0,
+            "right_hip_joint": 0.65,
+            "right_knee_joint": -1.25,
+            "right_wheel_joint": 0.0,
+        }
 
-task_registry.register(
-    "wheel_legged", LeggedRobot, WheelLeggedCfg(), WheelLeggedCfgPPO()
-)
-task_registry.register(
-    "wheel_legged_vmc", LeggedRobotVMC, WheelLeggedVMCCfg(), WheelLeggedVMCCfgPPO()
-)
-task_registry.register(
-    "wheel_legged_vmc_flat",
-    LeggedRobotVMC,
-    WheelLeggedVMCFlatCfg(),
-    WheelLeggedVMCFlatCfgPPO()
-)
-task_registry.register(
-    "diablo",
-    Diablo,
-    DiabloCfg(),
-    DiabloCfgPPO()
-)
+    class terrain(LeggedRobotCfg.terrain):
+        mesh_type = "plane"
+    class control(LeggedRobotCfg.control):
+        pos_action_scale = 0.5
+        vel_action_scale = 10.0
+        # PD Drive parameters:
+        stiffness = {"hip": 30.0, "knee": 30.0, "wheel": 0}  # [N*m/rad]
+        damping = {"hip": 0.5, "knee": 0.5, "wheel": 0.8}  # [N*m*s/rad]
+
+    class asset(LeggedRobotCfg.asset):
+        file = "{WHEEL_LEGGED_GYM_ROOT_DIR}/resources/robots/diablo/urdf/diablo_issac_gym.urdf"
+        name = "diablo"
+        offset = 0.
+        l1 = 0.14
+        l2 = 0.14
+        penalize_contacts_on = ["left", "right", "base"]
+        terminate_after_contacts_on = ["base"]
+        self_collisions = 1  # 1 to disable, 0 to enable...bitwise filter
+        flip_visual_attachments = False
+
+    class rewards(LeggedRobotCfg.rewards):
+        class scales(LeggedRobotCfg.rewards.scales):
+            nominal_state = -0.1
+            # action_smooth = -0.05
+
+class DiabloCfgPPO(LeggedRobotCfgPPO):
+    class runner(LeggedRobotCfgPPO.runner):
+        # logging
+        experiment_name = "diablo"
